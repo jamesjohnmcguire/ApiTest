77810ec39de3a93e09afbe2087924284c8eb68e1
James John McGuire <jamesjohnmcguire@gmail.com>
2025-05-01 21:49:12 +0900

    Update Copyright Years to Include 2025

--- SourceCode/APITester.php
+++ SourceCode/APITester.php
@@ -5,7 +5,7 @@
  *
  * @package   APITest
  * @author    James John McGuire <jamesjohnmcguire@gmail.com>
- * @copyright 2023 - 2024 James John McGuire <jamesjohnmcguire@gmail.com>
+ * @copyright 2023 - 2025 James John McGuire <jamesjohnmcguire@gmail.com>
  * @license   MIT https://opensource.org/licenses/MIT
  * @version   1.4.13
  * @link      https://github.com/jamesjohnmcguire/ApiTest
