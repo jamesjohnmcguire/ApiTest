df73351dd5eafbbada9d3670dd55f1af8467ca25
James John McGuire <jamesjohnmcguire@gmail.com>
2025-07-16 11:13:54 +0900

    Add Direct Option

--- DevelopmentTools/build.cmd
+++ DevelopmentTools/build.cmd
@@ -5,7 +5,7 @@ CD ..
 CALL composer validate --strict
 CALL composer install --prefer-dist
 ECHO outdated:
-CALL composer outdated
+CALL composer outdated --direct
 
 ECHO Checking code styles...
 php vendor\bin\phpcs -sp --standard=ruleset.xml SourceCode
