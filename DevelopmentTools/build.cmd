b3dde3271260731ffe8132899d16f518afa46004
James John McGuire <jamesjohnmcguire@gmail.com>
2025-07-16 12:24:36 +0900

    Add or Improve Release Support in Build Scripts

--- DevelopmentTools/build.cmd
+++ DevelopmentTools/build.cmd
@@ -15,4 +15,20 @@ ECHO Testing...
 CD DevelopmentTools
 CALL UnitTests.cmd
 
+IF "%1"=="release" GOTO release
+GOTO finish
+
+:release
+ECHO Release is set...
+if "%~2"=="" GOTO error
+
+IF EXIST digitalzenworks-apitest.zip DEL /Q digitalzenworks-apitest.zip
+7z a digitalzenworks-apitest.zip . -x!.git -x!.vscode -x!vendor -x!ApiTest.code-workspace
+
+gh release create v%2 --notes %2 digitalzenworks-apitest.zip
+DEL /Q digitalzenworks-apitest.zip
+
+:error
+ECHO No version tag supplied for release
+
 :finish
--- DevelopmentTools/build.sh
+++ DevelopmentTools/build.sh
@@ -13,15 +13,28 @@ vendor/bin/phpcs -sp --standard=ruleset.xml SourceCode
 
 vendor/bin/phpstan.phar analyse
 
-vendor/bin/phpunit -c Tests/phpunit.xml "$@"
+vendor/bin/phpunit -c Tests/phpunit.xml
 
 if [[ $1 == "release" ]] ; then
-	echo "Release Is Set!"
+	echo "Release is set..."
+
+	if [ -z "$2" ] ;then
+		echo "No version tag supplied for release"
+		exit 1
+	fi
 
 	# rm -rf Documentation
 	# phpDocumentor.phar --setting="graphs.enabled=true" -d SourceCode -t Documentation
 
-	zip -r digitalzenworks-apitest.zip .
-	gh release create v$2 --notes $2 digitalzenworks-apitest.zip
-	rm digitalzenworks-apitest.zip
+	file="digitalzenworks-apitest.zip"
+
+	if [ -f "$file" ] ; then
+		echo "Removing existing zip file..."
+		rm "$file"
+	fi
+
+	zip -r "$file" . -x ".git/*" -x ".vscode/*" -x "vendor/*" -x "ApiTest.code-workspace"
+
+	gh release create v$2 --notes $2 "$file"
+	rm "$file"
 fi
