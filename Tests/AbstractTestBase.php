5e5919117408bbadc3f954c5963d61e61ebb2234
James John McGuire <jamesjohnmcguire@gmail.com>
2025-07-23 16:35:30 +0900

    Bump Versions

--- Tests/AbstractTestBase.php
+++ Tests/AbstractTestBase.php
@@ -7,7 +7,7 @@
  * @author    James John McGuire <jamesjohnmcguire@gmail.com>
  * @copyright 2023 - 2025 James John McGuire <jamesjohnmcguire@gmail.com>
  * @license   MIT https://opensource.org/licenses/MIT
- * @version   1.6.32
+ * @version   1.7.34
  * @link      https://github.com/jamesjohnmcguire/ApiTest
  */
 
