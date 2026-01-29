4fd6b935c1dbfa6b98dacf42662aab3781d25c50
James John McGuire <jamesjohnmcguire@gmail.com>
2026-01-29 11:36:57 +0900

    Add Forgotten Bootstrap File

---
+++ Tests/Bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Bootstrap file.
+ *
+ * @package   APITest
+ * @author    James John McGuire <jamesjohnmcguire@gmail.com>
+ * @copyright 2023 - 2026 James John McGuire <jamesjohnmcguire@gmail.com>
+ * @license   MIT https://opensource.org/licenses/MIT
+ * @version   1.7.35
+ * @link      https://github.com/jamesjohnmcguire/ApiTest
+ */
+
+declare(strict_types=1);
+
+namespace DigitalZenWorks\ApiTest\Tests;
+
+$root = dirname(__DIR__, 1);
+
+require_once $root . '/vendor/autoload.php';
+require_once 'AbstractTestBase.php';
